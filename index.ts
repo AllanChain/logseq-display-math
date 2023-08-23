95afd912018b89719d7f92cc53711f394c19e5e0
Allan Chain <36528777+AllanChain@users.noreply.github.com>
2023-08-23 16:55:06 +0800

    feat: support custom KaTeX macros

--- index.ts
+++ index.ts
@@ -2,11 +2,26 @@ import '@logseq/libs'
 import type {KatexOptions} from 'katex'
 
 function main () {
+  logseq.useSettingsSchema([
+    {
+      title: 'KaTeX Macros',
+      description: 'LaTeX macros [with KaTeX](https://katex.org/docs/options)',
+      key: 'macros',
+      type: 'object',
+      default: {
+        '\\differentialD': '\\mathrm{d}',
+      },
+    },
+  ])
   logseq.Experiments.registerExtensionsEnhancer('katex', async (katex) => {
     const render = katex.render
     katex.render = (expression: string, baseNode: Node, options: KatexOptions) => {
       if (!options.displayMode) {
         expression = '\\displaystyle ' + expression
+      }
+      options.macros = {
+        ...options.macros,
+        ...logseq.settings?.macros,
       }
       return render(expression, baseNode, options)
     }
